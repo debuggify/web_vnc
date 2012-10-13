57959893dbd9b0cd6c163281fa4a7f10431fdf12
ciju cherian <ch3rian.ciju@gmail.com>
2012-10-14 00:28:21 +0530

    remove node_modules

---
+++ public/js/remote-client.js
@@ -0,0 +1,42 @@
+(function ($, now) {
+  $(document).ready(function(){
+
+    now.receiveMessage = function(name, message, byAgent){
+      byAgent = typeof byAgent !== undefined ? byAgent : "unknow";
+      $("#messages").append("<br><b>[" + byAgent + "]" + name + "</b>: " + message);
+    };
+
+    $("#send-all-developers").click(function(){
+      now.distributeMessage('developers', $("#text-input").val());
+      $("#text-input").val("");
+    });
+
+    $("#send-all-clients").click(function(){
+      now.distributeMessage('clients', $("#text-input").val());
+      $("#text-input").val("");
+    });
+
+    $("#send-all").click(function(){
+      now.distributeMessage('clients', $("#text-input").val());
+      now.distributeMessage('developers', $("#text-input").val());
+      $("#text-input").val("");
+    });
+
+    $(".change").click(function(){
+      now.changeRoom($(this).text());
+    });
+
+    $("#text-input").keypress(function (e) {
+      if (e.which && e.which === 13) {
+        $("#send-button").click();
+        return false;
+      }
+      return false;
+    });
+
+    now.name = prompt("What's your name?", "");
+
+    $("#text-input").focus();
+
+  });
+})(jQuery, now);
---
+++ public/js/remote-dev.js
@@ -0,0 +1,7 @@
+(function ($, now){
+  now.ready(function(){
+    console.log('ready with now');
+    now.logStuff("Hello World!");
+  });
+
+})(jQuery, now);
